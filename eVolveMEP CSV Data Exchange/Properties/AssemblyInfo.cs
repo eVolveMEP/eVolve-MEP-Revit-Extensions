4138386af2ebdee7b1be204079dc27915e71f1ed
Jason Faulkner <jfaulkner@asti.com>
2021-07-30 17:31:24 -0400

    1.0.2 Release

--- eVolveMEP CSV Data Exchange/Properties/AssemblyInfo.cs
+++ eVolveMEP CSV Data Exchange/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //      Build Date (MMDDY)
 //
-[assembly: AssemblyVersion("1.0.1.07231")]
-[assembly: AssemblyFileVersion("1.0.1.07231")]
+[assembly: AssemblyVersion("1.0.2.07301")]
+[assembly: AssemblyFileVersion("1.0.2.07301")]
